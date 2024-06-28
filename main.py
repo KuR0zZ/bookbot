b4301e0e70cbe00a9977f3362bb15ba0fd34e435
KuR0zZ <ferdinandiphone6@gmail.com>
2024-06-28 19:09:25 +0700

    create a function to count characters in a file

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ def main():
     book_path = "./books/frankenstein.txt"
     text = get_book_content(book_path)
     words = get_number_words(text)
+    characters = get_char_appear(text)
 
 def get_book_content(path):
     with open(path) as f:
@@ -10,6 +11,16 @@ def get_book_content(path):
 def get_number_words(text):
     words = text.split()
     return len(words)
+
+def get_char_appear(text):
+    character = {}
+    lowercase_string = text.lower()
+    for char in lowercase_string:
+        if char in character:
+            character[char] += 1
+        else:
+            character[char] = 1
+    return character
     
 
 main()
