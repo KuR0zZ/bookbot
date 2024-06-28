46c86d3a6da578adcf5071f71f393e080f092ab1
KuR0zZ <ferdinandiphone6@gmail.com>
2024-06-28 19:12:44 +0700

    create a function that sorts character based on number of occurences & codes that print out a report

--- main.py
+++ main.py
@@ -3,6 +3,16 @@ def main():
     text = get_book_content(book_path)
     words = get_number_words(text)
     characters = get_char_appear(text)
+    sorted_list = sort_list(characters)
+
+    print("--- Begin report of books/frankenstein.txt ---")
+    print(f"{words} words found in the document")
+    print()
+
+    for item in sorted_list:
+        print(f"The '{item["character"]}' character was found {item["occurence"]} times")
+    
+    print("--- End report ---")
 
 def get_book_content(path):
     with open(path) as f:
@@ -21,6 +31,17 @@ def get_char_appear(text):
         else:
             character[char] = 1
     return character
+
+def sort_on(dict):
+    return dict["occurence"]
+
+def sort_list(characters):
+    sorted_list = []
+    for character in characters:
+        if character.isalpha():
+            sorted_list.append({"character": character, "occurence": characters[character]})
+    sorted_list.sort(reverse=True, key=sort_on)
+    return sorted_list
     
 
 main()
